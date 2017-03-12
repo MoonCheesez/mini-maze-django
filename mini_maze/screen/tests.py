114a617c90394b9945da00c28e0df75cf471845f
MoonCheesez <joeltiojinhon@gmail.com>
2017-03-12 14:26:59 +0800

    Added tests for the screen app.

--- mini_maze/screen/tests.py
+++ mini_maze/screen/tests.py
@@ -1,3 +1,32 @@
+from settings import height, width, joined_players, max_players
+
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-# Create your tests here.
+from screen.views import screen
+
+class ScreenTest(TestCase):
+    def test_root_url_resolves_to_screen_page_view(self):
+        root = resolve('/')
+        self.assertEqual(root.func, screen)
+
+    def test_screen_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = screen(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('html'))
+        self.assertIn('<title>Mini Maze</title>', html)
+
+        if joined_players() == max_players:
+            self.assertIn('<section id="maze">', html)
+            
+            self.assertEqual(html.count('<tr>'), height)
+            self.assertEqual(html.count('<td>'), width)
+        else:
+            self.assertIn('<h1>Waiting for {0} more players to join...</h1>'
+                .format(max_players-joined_players()), html)
+
+            self.assertIn('<section id="waiting">', html)
+
+        self.assertTrue(html.endswith('html'))
