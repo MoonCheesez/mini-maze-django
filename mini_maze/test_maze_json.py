9d7ec97061450bacb03b452b834e24dbf432c19a
MoonCheesez <joeltiojinhon@gmail.com>
2017-03-14 11:06:37 +0800

    Added more tests for players and maze json Tests now rely on a setup python file which will create the json files Added tests for maze json to check if the json has the correct fields and check if the maze has valid values, 1 or 0 Changed the players test for having fields to be more readable Added a test for players to check if all players have unique positions Added a test for players to check if players are in the maze Edited players joined test for within setting limits to use assertLessEqual instead of assertTrue

--- mini_maze/test_maze_json.py
+++ mini_maze/test_maze_json.py
@@ -1,12 +1,24 @@
 from django.test import TestCase
 
 from settings import height, width, maze_json_filename
+from setup import reset_maze_json
+
 import json
 
 class MazeJSONTest(TestCase):
     def setUp(self):
+        reset_maze_json()
+
         with open(maze_json_filename, "r") as f:
             self.maze_json = json.load(f)
+
+    def test_have_fields(self):
+        self.assertIsNotNone(self.maze_json["maze"])
+
+    def test_maze_values(self):
+        for y in self.maze_json["maze"]:
+            for x in y:
+                self.assertIn(x, [0, 1])
 
     def test_dimensions(self):
         self.assertEqual(len(self.maze_json["maze"]), height)
--- mini_maze/test_players_json.py
+++ mini_maze/test_players_json.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 
-from settings import players_json_filename, maze_json_filename, max_players
+from settings import *
+from setup import reset_players_json, reset_maze_json
+
 import json
 
 class PlayersJSONTest(TestCase):
     def setUp(self):
+        reset_players_json()
+        reset_maze_json()
+
         with open(players_json_filename, "r") as f:
             self.players_json = json.load(f)
 
@@ -12,23 +17,42 @@ class PlayersJSONTest(TestCase):
             self.maze_json = json.load(f)
 
     def test_have_fields(self):
-        self.assertIsNotNone(self.players_json["player_positions"])
-        self.assertIsNotNone(self.players_json["players_joined"])
-        self.assertIsNotNone(self.players_json["move_number"])
-        self.assertIsNotNone(self.players_json["moves"])
+        fields = [
+            "player_positions",
+            "players_joined",
+            "move_number",
+            "moves"
+        ]
+
+        for field in fields:
+            self.assertIsNotNone(self.players_json[field])
+
+        self.assertEqual(len(self.players_json["player_positions"]), max_players)
 
     def test_players_have_x_and_y_position(self):
         for player in self.players_json["player_positions"]:
             self.assertEqual(len(player), 2)
 
+    def test_players_unique_positions(self):
+        positions = [tuple(x) for x in self.players_json["player_positions"]]
+        self.assertEqual(len(set(positions)),
+            len(self.players_json["player_positions"]))
+
     def test_players_not_in_walls(self):
-        for player in self.players_json["player_positions"]:
-            x = player[0]
-            y = player[1]
+        for position in self.players_json["player_positions"]:
+            x = position[0]
+            y = position[1]
 
             # 0 is a wall
             self.assertNotEqual(self.maze_json["maze"][y][x], 0)
 
-    def test_players_joined_within_settings_limits(self):
-        self.assertTrue(self.players_json["players_joined"] <= max_players)
+    def test_players_in_maze(self):
+        for position in self.players_json["player_positions"]:
+            x = position[0]
+            y = position[1]
 
+            self.assertLess(x, width)
+            self.assertLess(y, height)
+
+    def test_players_joined_within_settings_limits(self):
+        self.assertLessEqual(self.players_json["players_joined"], max_players)
