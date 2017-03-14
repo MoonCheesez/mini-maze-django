b2c97e2153d2ce2df692278ab67b25631355a104
MoonCheesez <joeltiojinhon@gmail.com>
2017-03-14 12:52:27 +0800

    Updated players views to pass tests

--- mini_maze/players/views.py
+++ mini_maze/players/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from settings import players_json_filename
+from setup import reset_all
+
+import json
+
 # Create your views here.
+def join(request):
+    with open(players_json_filename, "r") as f:
+        players = json.load(f)
+
+    try:
+        i = players["players_joined"].index(False)
+        players["players_joined"][i] = True
+
+        with open(players_json_filename, "w") as f:
+            json.dump(players, f)
+
+        return HttpResponse(i+1)
+    except ValueError:
+        return HttpResponse("max")
+
+def leave(request, player_id):
+    with open(players_json_filename, "r") as f:
+        players = json.load(f)
+
+    players["players_joined"][player_id-1] = False
+
+    with open(players_json_filename, "w") as f:
+        json.dump(players, f)
+
+def reset(request):
+    reset_all()
