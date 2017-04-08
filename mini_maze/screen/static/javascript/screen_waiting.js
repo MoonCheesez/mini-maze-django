9a086ee9826763ee743c64f501e10d91eed4d88c
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-08 14:29:38 +0800

    Added new styling for waiting for players. Functionality not finished.

--- mini_maze/screen/static/javascript/screen_waiting.js
+++ mini_maze/screen/static/javascript/screen_waiting.js
@@ -1,3 +1,30 @@
-setTimeout(function(){
-   window.location.reload(1);
-}, 5000);
+$(document).ready(function() {
+    // Set the colour of the number of players left
+    var colours = ["#FFB98B", "#FFBAE4", "#C491F1", "#00CDF1"];
+    
+    var playersLeftDOM = document.getElementById("players-left");
+    var playersLeft = parseInt(playersLeftDOM.textContent);
+    
+    var colour = colours[playersLeft-1];
+    playersLeftDOM.style.color = colour;
+
+    playersLeftDOM.style.visibility = "visible";
+
+    // Check for updates
+    var playersJoined;
+    window.setInterval(function() {
+        var newPlayersJoined;
+        $.getJSON("/players", function(d) {
+            var tmp = d["players_joined"];
+            //tmp.count(true)
+            newPlayersJoined = tmp.reduce(function(total,x){return x==true ? total+1 : total}, 0);
+        });
+
+        if (newPlayersJoined != playersJoined) {
+            // animate
+            playersJoined = newPlayersJoined;
+            document.getElementById("players-left").textContent = playersJoined.toString;
+        }
+
+    }, 500);
+});
