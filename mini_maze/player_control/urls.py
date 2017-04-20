d79d6b8e0fa2a3064fdbe51b3fb9e2d6c20ca5cf
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-20 23:05:45 +0800

    Updated url routing and context to have player number

--- mini_maze/player_control/urls.py
+++ mini_maze/player_control/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^controller/$', views.player_control, name='player_control-player_control'),
+    url(r'^controller/player([1-4])$', views.player_control, name='player_control-player_control'),
 ]
--- mini_maze/player_control/views.py
+++ mini_maze/player_control/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 from settings import players_json_filename, max_players, height, width
 
-def player_control(request):
+def player_control(request, player_no):
     context = {
-        "height": "0"*height,
-        "width": "0"*width,
+        "height": "0"*int(height/2),
+        "width": "0"*int(width/2),
+        "player_no": player_no
     }
     return render(request, 'player_control/player_control.html', context)
